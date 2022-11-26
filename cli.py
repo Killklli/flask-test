e23f4250e9d4decfb402c3201d87d87546504a8b
Thomas Vatter <11064610+Killklli@users.noreply.github.com>
2022-11-25 21:14:45 -0500

    loaded_data

--- cli.py
+++ cli.py
@@ -5,7 +5,7 @@ import json
 import pickle
 import random
 import sys
-
+import os
 from randomizer.Fill import Generate_Spoiler
 from randomizer.Settings import Settings
 from randomizer.SettingStrings import decrypt_setting_string
@@ -24,21 +24,25 @@ def generate(generate_settings, file_name):
 
 def main(arg1, arg2):
     """CLI Entrypoint for generating seeds."""
-    presets = json.load(open("static/presets/preset_files.json"))
-    default = json.load(open("static/presets/default.json"))
-    found = False
-    for file in presets.get("progression"):
-        with open("static/presets/" + file, "r") as preset_file:
-            data = json.load(preset_file)
-            if "Season 1 Race Settings" == data.get("name"):
-                setting_data = default
-                for key in data:
-                    setting_data[key] = data[key]
-                setting_data.pop("name")
-                setting_data.pop("description")
-                found = True
-    if found is False:
-        sys.exit(2)
+    # presets = json.load(open("static/presets/preset_files.json"))
+    # default = json.load(open("static/presets/default.json"))
+    # found = False
+    # for file in presets.get("progression"):
+    #     with open("static/presets/" + file, "r") as preset_file:
+    #         data = json.load(preset_file)
+    #         if "Season 1 Race Settings" == data.get("name"):
+    #             setting_data = default
+    #             for key in data:
+    #                 setting_data[key] = data[key]
+    #             setting_data.pop("name")
+    #             setting_data.pop("description")
+    #             found = True
+    # if found is False:
+    #     sys.exit(2)
+    loaded_data = os.environ["POST_BODY"]
+    print(loaded_data)
+    setting_data = json.loads(loaded_data)
+    print(setting_data)
     setting_data["seed"] = random.randint(0, 100000000)
     generate(setting_data, "out.bps")
 
